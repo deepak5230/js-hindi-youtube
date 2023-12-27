14425f9309fc917ebfe9cec110bbcf4e3de384af
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-27 14:39:24 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 123444
+let accountEmail = "deepakgupta@gmail.com"
+var accountPassword = "1234"
+accountcity = "jaipur"
+
+
+accountEmail= "ggolu@gmail.com"
+accountPassword="234232"
+accountCity= "patna"
+
+console.log([accountEmail,accountPassword,accountcity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+
+console.log(3+3)
+console.log("deepak")
+
+let name= "deepak"
+let age= 18
+let isloggedIn= false
+
+console.log(typeof age);
