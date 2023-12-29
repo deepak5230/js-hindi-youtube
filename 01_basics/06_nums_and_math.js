743cc0ecebd68a12cd9a769cb6f1f2ae2a66f37c
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-29 08:00:14 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,15 @@
+/*const score = 400
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString());*/
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.2))
+
+
