fbf3b76afff222ef26f921c329fda75720f9d59c
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-29 10:51:39 +0000

    array

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,54 @@
+
+//const myArr= [ 0,1,2,3,4,5]
+
+//const myheroes= ["shaktiman", "irnman", "pikashu"]
+
+//const myArr1 =  new Array(1,2,3,4);
+
+//myArr.push(6);
+//myArr.pop()
+
+//myArr.shift(9)
+//myArr.unshift()
+
+//console.log( myArr)
+//onst newArr = myArr.joint()
+//console.log(newArr)
+//console.log( myArr)
+
+//console.log("A",myArr);
+
+//const myn1= new Slice(1,3)
+
+
+//console.log("A" ,myArr);
+
+//let myn1 = myArr.slice(1 , 3)
+
+//console.log( myArr);
+//console.log(myn1);
+
+//let myn2 = myArr. splice()
+
+/*const myArr = [0,1,2,3,4,5]
+const myn2 = myArr.slice(1,3)
+const myn1 = myArr.splice(1,3)
+
+console.log(myn2)
+console.log(myn1)*/
+
+const myArr= [0,1,2,3,4,5]
+
+const myn1 = myArr. slice(1,3)
+
+
+
+const myn2 = myArr.splice(1, 3)
+
+console.log("A",  myn1)
+
+console.log( "B",  myn2);
+
+
+
+
