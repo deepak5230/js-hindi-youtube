c12a0d6d5776db2651829422962f3017c9c3bbdb
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-29 09:39:22 +0000

    datain

---
+++ 01_basics/02_basics/01_array.js
@@ -0,0 +1,7 @@
+const myArr= [1,2,3,4,5,6];
+
+const myheroes= ["shaktiman", "ironman", "superman",];
+
+const myarr= new Array(1,2,3,4)
+
+console.log
---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,16 @@
+let myDate= new Date()
+
+console.log(myDate);
+console.log(myDate.toDateString());
+
+console.log(myDate.toString());
+//let mycreatedDate= new Date( 23, 0, 2023)
+
+//console.log(mycreatedDate.toString());
+//let mycreatedDate = new Date("2012-1- 29 - 5-4");
+
+console.log( mycreatedDate.toLocaleString());
+
+let mycreatedDate = new Date( "2023- 12-2")
+console.log(mycreatedDate.getDate());
+
