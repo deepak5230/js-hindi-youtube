cc99cb90ce0b99278b1c704317fe7f0ee5678f32
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-30 09:46:12 +0000

    object

---
+++ .devcontainer/02_basics/03_object.js
@@ -0,0 +1,55 @@
+
+///// create an object and acces the value 
+const jsuser = {
+
+    name: "deepak",
+    age: 27,
+    mySym:"key1",
+    location: "Patna",
+    email: "deepakgupta351@gmail.com",
+
+    isLoggedIn: false,
+    lastlogindays: ["monday", "satuarday"],}
+
+    console.log( jsuser.name);
+    console.log(jsuser.age);
+    console.log(jsuser.location);
+
+    jsuser.email= "goligupta";
+    console.log(jsuser)
+
+    ///add function 
+    jsuser.greeeting= function(){
+
+        console.log("hello js user")
+
+    }
+    console.log(jsuser.greeting());//it is not a function how to create a function
+
+    const jsUser = {
+
+        name: "deepak"
+       // age; "28"
+        //location:"patna"
+
+
+    }
+
+    console.log(jsUser.name)
+
+ jsUser.name= "golu"
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ .devcontainer/02_basics/04_object.js
@@ -0,0 +1,9 @@
+const obj = { 1: "a", 2: "b", 3: "c"}
+
+const obj2 = { 4: "d", 5: "e", 6: "f"}
+
+
+// this is spreading method to imerge a object
+const obj3 = {...obj,...obj2}
+console.log(obj3);
+
