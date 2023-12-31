e41eb3af6338366b9c6fcd80ad80971c50353c90
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2023-12-31 12:42:06 +0000

    object.js

--- .devcontainer/02_basics/04_object.js
+++ .devcontainer/02_basics/04_object.js
@@ -4,6 +4,27 @@ const obj2 = { 4: "d", 5: "e", 6: "f"}
 
 
 // this is spreading method to imerge a object
-const obj3 = {...obj,...obj2}
+//const obj3 = {...obj,...obj2}
+//console.log(obj3);
+//const obj3= { obj, obj2}
+
+const obj3= object.assign({} , obj,obj2)
 console.log(obj3);
+
+const course = {
+
+   coursename: "javascript",
+    price: "999",
+    courseInstructor: "deepak",
+}
+
+//course.courseInstructor// destructure the value  const{courseInstructor: Instructor}= course
+const {courseInstructor: Instructor}= course
+
+console.log(Instructor);
+
+
+
+
+
 
