2709893a417968d54853b401b4c9ad1eb097c15c
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-01-02 07:01:11 +0000

    functon

---
+++ .devcontainer/02_basics/03_basics/01_function.js
@@ -0,0 +1,132 @@
+/*function mysayName(){
+
+    console.log("deepak")
+    console.log("golu")
+    console.log("hhhhh")
+}
+
+    mysayName()
+function addtwonumber(number1, number2){
+
+    console.log(number1 + number2)
+    const result = number1 + number 2
+    return result/// return krne ke baad console.log kav kaam nhi krega
+}
+const result = addtwonumber(3, "a")
+
+//console.log("Result:",   result)
+
+function loginusermsg(username){
+    ///if (iske ander compare krna hai){
+        //executes krna hai iske ander
+    }
+
+    if(username=== undefined){
+
+        console.log("please enter a username")
+        return
+    }
+    return '${username} just logged in'
+
+    function mysayName( number 1, number2){
+
+        mysayName(3, 4)
+    }*/
+
+    //let reskdkncjukt = number1+ number2
+     //console.log( let sreshjbh)
+
+
+
+
+
+//console.log(loginusermsg("deepak"))
+
+/*function addtwonumber(number1, number2){
+
+    console.log(number1 +number2)
+
+    const result = number1+ number2
+
+    return(result)
+
+
+}
+
+const result = addtwonumber(5, 5)*/
+
+// create an object and passed the data types
+//iss object ko function me pass kia jata hai
+
+/*const user = {
+
+    username: "deepak",
+
+    price: 23
+
+}
+
+function handleObject(anyobject){
+
+    console.log('username is ${anyobject.username} and price is ${anyobject.price}');
+}
+
+    //handleObject(user)
+
+    handleObject({
+        username: "sam",
+        price: 233
+
+    })
+    // create an array and pass the object
+    const myArray =[ 100,200,300,400];
+
+
+    function puchuobject(myArray){
+
+        return myArray[0]
+    }
+
+    console.log(puchuobject(myArray));
+
+     //
+     const myArray1 = [222,333,4344,444];
+
+     function ppp(getvalue){
+
+        return myArray1[0]
+     }
+     console.log(ppp(getvalue));*/
+
+
+     const user ={
+
+        username: "deepk",
+
+        price: 299
+
+     }
+
+     function tuk(anybody){
+
+        console.log( 'username is ${anybody.username and price is ${anybody.price}')
+
+        
+     }
+        tuk(user)
+        /*
+        const user = {
+
+            name: "deepak"
+            price: 199
+        }
+
+        function tuk(anybody){
+
+            console.log('user name is ${anybody.name } and price is {anybody.price})
+        }
+
+        tuk(anybody)
+
+
+
