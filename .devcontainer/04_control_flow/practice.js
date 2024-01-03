eb68758a4e58c8aeebfa450bed24d55ef239bf68
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-01-03 18:09:28 +0000

    practicde

---
+++ .devcontainer/04_control_flow/practice.js
@@ -0,0 +1,72 @@
+
+const balance = 100
+
+if(balance>500){
+
+    console.log( "executed")
+}
+else if(balance>750){
+
+    console.log("maintain")
+}
+
+else if(balance<800){
+
+    console.log("bank")
+}
+else{
+    console.log("noteexecuted")
+}
+
+console.log("iron man")
+
+const userLoggedin = true
+const debitcard = true
+
+if( userLoggedin&& debitcard ){
+
+    console.log( "buyinng courses")
+}
+const userLoggedingoogle= true
+const userLoggedingmail= true
+
+if(userLoggedingoogle || userLoggedingmail){
+
+    console.log("allow all data")
+}
+   
+const month = 3
+
+switch(month){
+
+    case 1:
+        console.log("january")
+        break;
+
+    case 2:
+        console.log("feb")
+        break;
+
+    case 3:
+        console.log("March")
+        break;
+
+    case 4:
+        console.log("april")
+        break;
+}
+
+const month1= "march"
+
+switch(month1){
+
+    case "jan":
+        console.log("january")
+        case "feb":
+            console.log("feb")
+            case "march":
+                console.log(march)
+                console.log("march")
+
+        
+}
